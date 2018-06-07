68074080180fecc410a8c3f8ac5991a5a94cee12
DarriusWrightGD <darriuswrightgd@gmail.com>
2018-06-06 21:12:42 -0600

    Output S3 url after deploy

--- .docker/deploy/nonprod.sh
+++ .docker/deploy/nonprod.sh
@@ -2,3 +2,4 @@ echo "Running the deploy script..."
 aws cloudformation create-stack --region ${AWS_REGION} --stack-name ${STACK_NAME} --template-body file://.aws/nonprod/template.yml --parameters ParameterKey=BucketName,ParameterValue=${BUCKET_NAME} ParameterKey=BranchName,ParameterValue=${BRANCH_NAME}
 aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME} --region ${AWS_REGION}
 aws s3 cp --recursive --acl=public-read /app/dist/ s3://${BUCKET_NAME} --region ${AWS_REGION}
+aws cloudformation describe-stacks --stack-name=${STACK_NAME} --query 'Stacks[0].Outputs[?OutputKey==`WebsiteURL`].OutputValue' --output=text
